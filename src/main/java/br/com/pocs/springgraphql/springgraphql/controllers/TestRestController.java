4ec03528b5f493155d117a5f883bb8688b068ff4
Lincoln Araujo <lincoln.n.araujo@gmail.com>
2022-02-09 10:13:28 -0300

    update> property para evitar erro por causa do lazy load

--- src/main/java/br/com/pocs/springgraphql/springgraphql/controllers/TestRestController.java
+++ src/main/java/br/com/pocs/springgraphql/springgraphql/controllers/TestRestController.java
@@ -1,7 +1,9 @@
 package br.com.pocs.springgraphql.springgraphql.controllers;
 
 import br.com.pocs.springgraphql.springgraphql.model.ClienteEntity;
+import br.com.pocs.springgraphql.springgraphql.model.CompraEntity;
 import br.com.pocs.springgraphql.springgraphql.repository.ClienteRepository;
+import br.com.pocs.springgraphql.springgraphql.service.CompraService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,8 @@ public class TestRestController {
 
     private final ClienteRepository clienteRepository;
 
+    private final CompraService compraService;
+
     @GetMapping("/clientes")
     public ResponseEntity<List<ClienteEntity>> getClientes(){
 
@@ -36,4 +40,11 @@ public class TestRestController {
         return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.of(Optional.empty()));
     }
 
+    @GetMapping("/compra/{id}")
+    public ResponseEntity<CompraEntity> getCompra(@PathVariable("id") final String id){
+
+        final var compra = compraService.findById(UUID.fromString(id));
+
+        return ResponseEntity.ok(compra);
+    }
 }
